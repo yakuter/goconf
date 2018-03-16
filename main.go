78f9dfec831378799c03297a5f8d82b770395699
Erhan Yakut <yakutair@Erhans-MacBook-Air.local>
2018-03-16 15:20:04 +0300

    updated

--- main.go
+++ main.go
@@ -6,38 +6,27 @@ import (
 	"os"
 )
 
-var animal, fruits, quantity = getConfig()
-
-// Configuration struct to handle variables
-type Configuration struct {
+// Conf struct to handle variables
+type Conf struct {
 	Animal   string
 	Fruits   []string
 	Quantity int
 }
 
-func getConfig() (string, []string, int) {
+func main() {
+
 	file, _ := os.Open("conf.json")
 	defer file.Close()
-
 	decoder := json.NewDecoder(file)
-	configuration := Configuration{}
-	err := decoder.Decode(&configuration)
+	conf := Conf{}
+	err := decoder.Decode(&conf)
 	if err != nil {
 		panic(err)
 	}
 
-	animal := configuration.Animal
-	fruits := configuration.Fruits
-	quantity := configuration.Quantity
-
-	return animal, fruits, quantity
-}
-
-func main() {
-
-	fmt.Println("Animal   :", animal)
-	fmt.Println("Fruits   :", fruits)
-	fmt.Println("Quantity :", quantity)
+	fmt.Println("Animal   :", conf.Animal)
+	fmt.Println("Fruits   :", conf.Fruits)
+	fmt.Println("Quantity :", conf.Quantity)
 
 	/* Output
 
